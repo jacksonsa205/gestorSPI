270d51a209679b55f965b17d8cbb101d91497b18
Jackson Alves De Sa <jackson.sa@telefonica.com>
2025-03-24 18:45:01 -0300

    [FIX] Ajuste na permissão do botão de cadastro do modul reporte REM

--- client/src/pages/GestaoObras/ReporteREM/ReporteREM.jsx
+++ client/src/pages/GestaoObras/ReporteREM/ReporteREM.jsx
@@ -560,7 +560,7 @@ const handleExcluirConsulta = async (rem) => {
               </InputGroup>
             </Col>
             <Col md={4} className="d-flex justify-content-end">
-            {permissions.canEdit && (
+            {permissions.canCadastro && (
               <Button variant="primary" onClick={() => setShowNovoModal(true)}>
                 <FontAwesomeIcon icon={faPlus} className="me-2" />
                 Cadastrar
